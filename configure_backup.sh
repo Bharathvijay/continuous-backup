40c68fe9001907d06f4cf8684312b74e100f11f6
Silenced BV <silenced@localhost.localdomain>
2017-02-06 19:35:14 +0530

    fixing filename

--- configure_backup.sh
+++ configure_backup.sh
@@ -4,4 +4,4 @@ kubectl create -f k8s/ConfigMap.yaml
 kubectl create -f k8s/Secrets.yaml
 kubectl replace -f k8s/Deployment-backup.yaml
 kubectl create -f k8s/Job-pg-backup.yaml
-kubectl create -f k8s/CronDeployment-pg-basebackup.yaml
+kubectl create -f k8s/CronDelpoyment-pg-basebackup.yaml
